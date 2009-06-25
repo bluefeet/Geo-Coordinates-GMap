b3e19d7e469d9d31c40a1e64f9f076063d3a9fea
Aran Deltac <bluefeet@gmail.com>
2009-06-25 13:19:49 -0700

    version 0.03

--- lib/Geo/Coordinates/GMap.pm
+++ lib/Geo/Coordinates/GMap.pm
@@ -8,7 +8,9 @@ Map tiles, and back again.
 =head1 SYNOPSIS
 
     use Geo::Coordinates::GMap;
-    my ($tile_x, $tile_y, $x, $y) = coord_to_tile( $lat, $lon, $zoom );
+    my ($tile_x, $tile_y) = coord_to_gmap_tile( $lat, $lon, $zoom );
+    my ($new_tile_x, $new_tile_y) = zoom_gmap_tile( $tile_x, $tile_y, $old_zoom, $new_zoom );
+    my ($x, $y) = gmap_tile_xy( $tile_x, $tile_y );
 
 =head1 DESCRIPTION
 
@@ -27,41 +29,33 @@ This module makes this proces simple and accurate.
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Math::Trig;
 
 use Exporter qw( import );
-our @EXPORT = qw( coord_to_tile );
+our @EXPORT = qw(
+    coord_to_gmap_tile
+    zoom_gmap_tile
+    gmap_tile_xy
+);
 
 =head1 FUNCTIONS
 
-=head2 coord_to_tile
+=head2 coord_to_gmap_tile
 
-    my ($tile_x, $tile_y, $x, $y) = coord_to_tile( $lat, $lon, $zoom );
+    my ($tile_x, $tile_y) = coord_to_gmap_tile( $lat, $lon, $zoom );
 
 Given a decimal latitude and longitude, and a google maps zoom level (0 being farthest away
-and 19 being the closest that I'm aware of that you can get), this function will return the
-following list of values:
-
-=over
-
-=item tile_x: The tiles x-coordinate.
-
-=item tile_y: The tiles y-coordinate.
-
-=item x: The x position of the pixel within the tile.
-
-=item y: The y position of hte pixel within the tile.
-
-=back
+and 20 being the closest that I'm aware of that you can get), this function will return the
+GMap tile location as a fractional x and y coordinate.
 
 =cut
 
 # Inspired by some C# code at:
 # http://groups.google.co.in/group/Google-Maps-API/browse_thread/thread/d2103ac29e95696f
 
-sub coord_to_tile {
+sub coord_to_gmap_tile {
     my ($lat, $lon, $zoom) = @_;
 
     # The C# code did this, but I don't know why, so I'm not going to enable it.
@@ -72,14 +66,59 @@ sub coord_to_tile {
     my $norm_x = $lon / 180;
     my $norm_y = (0.5 * log((1 + $sin_phi) / (1 - $sin_phi))) / pi;
 
-    my $y = (2 ** $zoom) * ((1 - $norm_y) / 2);
-    my $x = (2 ** $zoom) * (($norm_x + 1) / 2);
+    my $tile_x = (2 ** $zoom) * (($norm_x + 1) / 2);
+    my $tile_y = (2 ** $zoom) * ((1 - $norm_y) / 2);
 
     return(
-        int($x),
-        int($y),
-        int( (($x - int($x)) * 256) + 0.5),
-        int( (($y - int($y)) * 256) + 0.5),
+        $tile_x,
+        $tile_y,
+    );
+}
+
+=head2 zoom_gmap_tile
+
+    my ($new_tile_x, $new_tile_y) = zoom_gmap_tile( $tile_x, $tile_y, $old_zoom, $new_zoom );
+
+Converts fractional tile coordinates, as craated by coord_to_gmap_tile(), from one
+zoom level to another.
+
+=cut
+
+sub zoom_gmap_tile {
+    my ($tile_x, $tile_y, $old_zoom, $new_zoom) = @_;
+
+    if ($new_zoom < $old_zoom) {
+        foreach ($new_zoom .. ($old_zoom-1)) {
+            $tile_x = $tile_x / 2;
+            $tile_y = $tile_y / 2;
+        }
+    }
+    elsif ($new_zoom > $old_zoom) {
+        foreach (($old_zoom+1) .. $new_zoom) {
+            $tile_x = $tile_x * 2;
+            $tile_y = $tile_y * 2;
+        }
+    }
+
+    return( $tile_x, $tile_y );
+}
+
+=head2 gmap_tile_xy
+
+    my ($x, $y) = gmap_tile_xy( $tile_x, $tile_y );
+
+Given a tile's x and y coordinate as provided by coord_to_gmap_tile(), this function
+will return the pixel location within the tile.  This function will return a fractional
+number.  In most cases you will want to remove the fractional portion.
+
+=cut
+
+sub gmap_tile_xy {
+    my ($tile_x, $tile_y) = @_;
+
+    return(
+        ($tile_x - int($tile_x)) * 256,
+        ($tile_y - int($tile_y)) * 256,
     );
 }
 
--- t/01_basics.t
+++ t/01_basics.t
@@ -9,8 +9,26 @@ BEGIN {
 }
 
 {
-    my ($x, $y) = coord_to_tile( 86, 177, 1 );
-    is( $x, 1, 'tile x is 0' );
-    is( $y, 0, 'tile y is 1' );
+    my ($x, $y) = coord_to_gmap_tile( 86, 177, 1 );
+    is( int($x), 1, 'tile x is 0' );
+    is( int($y), 0, 'tile y is 1' );
+}
+
+{
+    my ($x, $y) = zoom_gmap_tile( 0.4, 0.6, 0, 1 );
+    is( int($x), 0, 'tile x is 0' );
+    is( int($y), 1, 'tile y is 1' );
+}
+
+{
+    my ($x, $y) = zoom_gmap_tile( 7.5, 12.3, 4, 1 );
+    is( int($x), 0, 'tile x is 0' );
+    is( int($y), 1, 'tile y is 1' );
+}
+
+{
+    my ($x, $y) = gmap_tile_xy( 1.50, 5.75 );
+    is( $x, 128, 'x is 128' );
+    is( $y, 192, 'y is 192' );
 }
 
